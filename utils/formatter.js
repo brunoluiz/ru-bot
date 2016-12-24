309d5adc7c708e897a07210f73c1c97cba7cb39d
Bruno Luiz <contact@brunoluiz.net>
2016-12-24 16:33:22 -0200

    Added a decent date formatting to the Formatter class

--- utils/formatter.js
+++ utils/formatter.js
@@ -15,7 +15,11 @@ const Formatter = {
       dessert: session.localizer.gettext(session.preferredLocale(), 'menu:format:dessert'),
     }
 
-    let menu = '### ' + locale.title + item.date + '\n\n';
+    let date = item.date.getUTCDate() + '/';
+    date += item.date.getUTCMonth() + '/';
+    date += item.date.getUTCFullYear();
+
+    let menu = '### ' + locale.title + date + '\n\n';
     menu += (item.basics) ? (label(locale.basics) + item.basics + '\n\n') : '';
     menu += (item.main_dish) ? (label(locale.main_dish) + item.main_dish + '\n\n') : '';
     menu += (item.side_dish) ? (label(locale.side_dish) + item.side_dish + '\n\n') : '';

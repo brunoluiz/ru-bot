13faed9c68eead51a5c201c1a02b15f2f206f092
Bruno Luiz <contact@brunoluiz.net>
2016-12-25 23:19:32 -0200

    Updated the intents

--- bot/intents.js
+++ bot/intents.js
@@ -4,13 +4,13 @@ const builder = require('botbuilder');
 const intents = new builder.IntentDialog();
 
 // Strings to match (and its dialogs)
-intents.matches(/^hoje/i, (session) => session.beginDialog('Menu:Today'));
-intents.matches(/^amanhã/i, (session) => session.beginDialog('Menu:Tomorrow'));
-intents.matches(/^semana/i, (session) => session.beginDialog('Menu:Week'));
-intents.matches(/^menu/i, (session) => session.beginDialog('Menu:Week'));
-intents.matches(/^assina/i, (session) => session.beginDialog('Subscribe:CheckStatus'));
-intents.matches(/^notifica/i, (session) => session.beginDialog('Subscribe:CheckStatus'));
-intents.matches(/^cancel/i, (session) => session.beginDialog('Subscribe:CheckStatus'));
+intents.matches(/hoje/ig, (session) => session.beginDialog('Menu:Today'));
+intents.matches(/amanhã/ig, (session) => session.beginDialog('Menu:Tomorrow'));
+intents.matches(/semana/ig, (session) => session.beginDialog('Menu:Week'));
+intents.matches(/menu/ig, (session) => session.beginDialog('Menu:Week'));
+intents.matches(/assina/ig, (session) => session.beginDialog('Subscribe:CheckStatus'));
+intents.matches(/notifica/ig, (session) => session.beginDialog('Subscribe:CheckStatus'));
+intents.matches(/cancel/ig, (session) => session.beginDialog('Subscribe:CheckStatus'));
 
 // Default action, when no match is found
 intents.onDefault((session, args, next) => session.beginDialog('Welcome:Greeting'));

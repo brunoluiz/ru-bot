a1f43c9544044434db0ed6393e64e1f566aeeaf4
Bruno Luiz <contact@brunoluiz.net>
2017-03-26 14:15:43 -0300

    Move foods images to a separated file

---
+++ bot/foods.js
@@ -0,0 +1,12 @@
+const url = process.env.URL || `http://localhost:${process.env.PORT}`;
+
+module.exports = [
+  `${url}/public/assets/images/food-1.jpg`,
+  `${url}/public/assets/images/food-2.jpg`,
+  `${url}/public/assets/images/food-3.jpg`,
+  `${url}/public/assets/images/food-4.jpg`,
+  `${url}/public/assets/images/food-5.jpg`,
+  `${url}/public/assets/images/food-6.jpg`,
+  `${url}/public/assets/images/food-7.jpg`,
+  `${url}/public/assets/images/food-8.jpg`,
+];
